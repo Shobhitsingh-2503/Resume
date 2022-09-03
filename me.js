fb4406ba21267dda92217d7977ceb3d7f23b0a4b
Shobhit Singh <95973429+IITianShobhitsingh@users.noreply.github.com>
2022-09-03 19:40:58 +0530

    Add files via upload

---
+++ me.js
@@ -0,0 +1,26 @@
+var pic = document.querySelector('.pics')
+pic.addEventListener('click',function(){
+    location.href = "load3.html";
+})
+
+var pro = document.querySelector('.proj')
+pro.addEventListener('click',function(){
+    location.href = "load2.html";
+})
+
+var contact = document.querySelector('.ABOUT')
+contact.addEventListener('click',function(){
+    location.href = "load4.html";
+})
+var home = document.querySelector('.Home')
+home.addEventListener('click',function(){
+    location.href = "load1.html";
+})
+var hello = document.querySelector('.me')
+hello.addEventListener('click',function(){
+    location.href = "load5.html";
+})
+var cv = document.querySelector('.resume')
+cv.addEventListener('click',function(){
+    location.href = 'C:/Users/Shobhit Singh/OneDrive/Desktop/Activities/resume/RESUME_SHOBHIT_SINGH.pdf'
+})
